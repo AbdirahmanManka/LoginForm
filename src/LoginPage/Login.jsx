e57a1c1e12a060b2317dfcd3d58bfd00d1239d37
Abdirahman <114359001+OmarAbdirahman@users.noreply.github.com>
2023-02-06 11:29:59 +0300

    added a crousel

--- src/LoginPage/Login.jsx
+++ src/LoginPage/Login.jsx
@@ -70,6 +70,36 @@ function Login() {
             
         </form>
         }
+        
+        <div id='slider'>
+     
+        <div className="carousel-wrapper">
+            <span id='item-1'></span>
+            <span id='item-2'></span>
+            <span id='item-3'></span>
+            <div className="carousel-item item-1">
+                <a href="#item-3" class="arrow-prev arrow"></a>
+                <a href="#item-2" class="arrow-next arrow"></a>
+
+                
+
+            </div>
+            <div className="carousel-item item-2">
+                <a href="#item-1" class="arrow-prev arrow"></a>
+                <a href="#item-3" class="arrow-next arrow"></a>
+
+                
+
+            </div>
+            <div className="carousel-item item-3">
+                <a href="#item-2" class="arrow-prev arrow"></a>
+                <a href="#item-1" class="arrow-next arrow"></a>
+
+                
+
+            </div>
+        </div>
+        </div>
 
 
     </div>
